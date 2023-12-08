515f4ccf75ddf782a040aa165a156c27b1d0cae0
Juan Manuel <juanmamtzx202@gmail.com>
2023-12-08 13:56:32 -0600

    obtene consultas

--- src/app/mostrar-productos/mostrar-productos.component.ts
+++ src/app/mostrar-productos/mostrar-productos.component.ts
@@ -9,11 +9,59 @@ import { ObtenerProductosService } from '../services/obtener-productos.service';
 export class MostrarProductosComponent implements OnInit {
   
   productos: any[] = [];
+  vista1: any[] = [];
+  vista2: any[] = [];
+  vista3: any[] = [];
+  clienteempleados: any[] =[];
+  totalcategoria: any[] =[];
+  totalPrecioProductos: any[] =[];
 
   constructor(private productoService: ObtenerProductosService) {}
 
   ngOnInit(): void {
     this.obtenerProductos();
+    this.obtenerVista1();
+    this.obtenerVista2();
+    this.obtenerVista3();
+    this.obtenerClienteEmpleado();
+    this.totalCategoria();
+    this.totalProductos();
+
+  }
+
+  totalProductos(): void {
+    this.productoService.totalPrecioProducto().subscribe(
+      (data) => {
+        this.totalPrecioProductos = data;
+      },
+      (error) => {
+        console.error('Error al obtener productos', error);
+      }
+    );
+  }
+
+
+
+  totalCategoria(): void {
+    this.productoService.totalcategoria().subscribe(
+      (data) => {
+        this.totalcategoria = data;
+      },
+      (error) => {
+        console.error('Error al obtener productos', error);
+      }
+    );
+  }
+
+  obtenerClienteEmpleado(): void {
+    this.productoService.clientessonempleados().subscribe(
+      (data) => {
+        this.clienteempleados = data;
+      },
+      (error) => {
+        console.error('Error al obtener productos', error);
+      }
+    );
   }
 
   obtenerProductos(): void {
@@ -26,4 +74,39 @@ export class MostrarProductosComponent implements OnInit {
       }
     );
   }
+
+  obtenerVista1(): void {
+    this.productoService.getvista1().subscribe(
+      (data) => {
+        this.vista1 = data;
+      },
+      (error) => {
+        console.error('Error al obtener productos', error);
+      }
+    );
+  }
+
+  obtenerVista2(): void {
+    this.productoService.getvista2().subscribe(
+      (data) => {
+        this.vista2 = data;
+      },
+      (error) => {
+        console.error('Error al obtener productos', error);
+      }
+    );
+  }
+
+  obtenerVista3(): void {
+    this.productoService.getvista3().subscribe(
+      (data) => {
+        this.vista3 = data;
+      },
+      (error) => {
+        console.error('Error al obtener productos', error);
+      }
+    );
+  }
+
+
 }
--- src/app/services/obtener-productos.service.ts
+++ src/app/services/obtener-productos.service.ts
@@ -15,4 +15,32 @@ export class ObtenerProductosService {
     return this.http.get<any[]>(`${this.apiUrl}/productos`);
   }
 
+  getvista1(): Observable<any[]> {
+    return this.http.get<any[]>(`http://localhost:3000/vista_empleado_usuario`);
+  }
+
+  getvista2(): Observable<any[]> {
+    return this.http.get<any[]>(`http://localhost:3000/vista_productos`);
+  }
+
+  getvista3(): Observable<any[]> {
+    return this.http.get<any[]>(`http://localhost:3000/vista_cliente_direccion`);
+  }
+
+  clientessonempleados(): Observable<any[]> {
+    return this.http.get<any[]>(`http://localhost:3000/clientes_son_empleados`);
+  }
+
+  totalcategoria(): Observable<any[]> {
+    return this.http.get<any[]>(`http://localhost:3000/total_por_categoria`);
+  }
+
+  
+
+  
+  totalPrecioProducto(): Observable<any[]> {
+    return this.http.get<any[]>(`http://localhost:3000/total_precio_productos`);
+  }
+  
+
 }
